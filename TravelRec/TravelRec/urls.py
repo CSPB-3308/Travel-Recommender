a0f684464d1664f7e2097385020ed6ec16a11761
Bryan Arment <bryan.arment@gmail.com>
2020-11-09 09:06:53 -0700

    Fixed url path issue for flights test:

--- TravelRec/TravelRec/urls.py
+++ TravelRec/TravelRec/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
     path('recommendations/', include('pages.urls')),
-]
+    path('flights/',include('flights.urls')),]
 
