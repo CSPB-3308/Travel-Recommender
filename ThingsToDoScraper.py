6ab36d2ace2a86a63bb552507551dc26f682b117
bmeares1331 <71229863+bmeares1331@users.noreply.github.com>
2020-12-06 20:57:35 -0700

    Update ThingsToDoScraper.py

--- ThingsToDoScraper.py
+++ ThingsToDoScraper.py
@@ -2,7 +2,7 @@ from time import sleep, strftime
 from random import randint
 import pandas as pd
 from selenium import webdriver
-import os
+#import os
 #from pathlib import Path
 # Source: https://towardsdatascience.com/if-you-like-to-travel-let-python-help-you-scrape-the-best-fares-5a1f26213086
 # mac chrome driver path:
@@ -24,53 +24,52 @@ options.add_argument('--headless')
 def scrapeForThingsToDo(location):
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
     sleep(2)
+
     """City codes - it's the city, state (in US) and city, country for international
     (spaces in city name and country name must be _) ex: Denver_CO, London_England"""
 
     usnews = ('https://travel.usnews.com/' + location + '/Things_To_Do/')
     driver.get(usnews)
     sleep(randint(8, 10))
-    try:
-        # sometimes a popup shows up, so we can use a try statement to check it and close
-        """try:
-            driver.implicitly_wait(10)
-            xp_popup_close = '//button[contains(@id,"dialog-close") and contains(@class,"Button-No-Standard-Style close ")]'
-            driver.find_elements_by_xpath(xp_popup_close)[-1].click()
-        except:
-            pass"""
-        # This function takes care of the scraping part
-        place = '// *[ @class="GenericList__ListItemContainer-tjuxmv-1 deINyp"]'
-        sections = driver.find_elements_by_xpath(place)
 
-        places_list = [value.text for value in sections]
-        parsed_places = []
-        for i in places_list:
-            parsed_places.append(i.split('\n'))
-        place_name = []
-        type_place = []
-        time_to_spend = []
-        summary = []
-        for attraction in parsed_places:
-            attraction.pop(0)  # remove first val in array
-            for info in attraction:
-                if info == 'FREE':
-                    attraction.remove(info)
-                if info == 'Find Tours & Tickets':
-                    attraction.remove(info)
-            place_name.append(attraction[0])
-            type_place.append(attraction[2])
-            time_to_spend.append(attraction[4])
-            summary.append(attraction[-1])
-        places_df = pd.DataFrame()
-        places_df['Attraction Name'] = place_name
-        places_df['Type'] = type_place
-        places_df['Time To Spend'] = time_to_spend
-        places_df['About'] = summary
-        places_df['Timestamp'] = strftime("%Y-%m-%d-%H:%M")  # so we can know when it was scraped
-        result = places_df.to_html()
-        driver.close()
-        driver.quit()
-    finally:  
-        driver.close()
-        driver.quit()
+    # sometimes a popup shows up, so we can use a try statement to check it and close
+    """try:
+        driver.implicitly_wait(10)
+        xp_popup_close = '//button[contains(@id,"dialog-close") and contains(@class,"Button-No-Standard-Style close ")]'
+        driver.find_elements_by_xpath(xp_popup_close)[-1].click()
+    except:
+        pass"""
+    # This function takes care of the scraping part
+    place = '// *[ @class="GenericList__ListItemContainer-tjuxmv-1 deINyp"]'
+    sections = driver.find_elements_by_xpath(place)
+
+    places_list = [value.text for value in sections]
+    parsed_places = []
+    for i in places_list:
+        parsed_places.append(i.split('\n'))
+    place_name = []
+    type_place = []
+    time_to_spend = []
+    summary = []
+    for attraction in parsed_places:
+        attraction.pop(0)  # remove first val in array
+        for info in attraction:
+            if info == 'FREE':
+                attraction.remove(info)
+            if info == 'Find Tours & Tickets':
+                attraction.remove(info)
+        place_name.append(attraction[0])
+        type_place.append(attraction[2])
+        time_to_spend.append(attraction[4])
+        summary.append(attraction[-1])
+    places_df = pd.DataFrame()
+    places_df['Attraction Name'] = place_name
+    places_df['Type'] = type_place
+    places_df['Time To Spend'] = time_to_spend
+    places_df['About'] = summary
+    places_df['Timestamp'] = strftime("%Y-%m-%d-%H:%M")  # so we can know when it was scraped
+    result = places_df.to_html()
+    driver.close()
+    driver.quit()
+    
     return result
